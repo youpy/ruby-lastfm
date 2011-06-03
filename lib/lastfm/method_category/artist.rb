f4ad02a44c97a5f878494c9d53f138e4644c2e1a
Rafael Sachetto <rafael.sachetto@gmail.com>
2011-06-03 08:14:10 -0700

    added another methods to artists (from user gadogado)

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -5,6 +5,18 @@ class Lastfm
         response.xml['events']['event']
       end
 
+      regular_method :get_top_tags, [:artist], [] do |response|
+        response.xml['toptags']['tag']
+      end
+
+      regular_method :get_top_albums, [:artist, :limit], [] do |response|
+        response.xml['topalbums']['album']
+      end
+
+      regular_method :get_images, [:artist, :limit, :order], [] do |response|
+        response.xml['images']['image']
+      end
+
       regular_method :search, [:artist], [[:limit, nil], [:page, nil]]
     end
   end
