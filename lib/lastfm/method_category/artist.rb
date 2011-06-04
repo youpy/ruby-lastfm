5879508be9c2188944374d95ce449b987a981e8a
Rafael Sachetto <rafaelsachetto@labyard.com>
2011-06-04 00:11:48 -0300

    added optinal parameters to methods on artists

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -1,23 +1,23 @@
 class Lastfm
   module MethodCategory
     class Artist < Base
-      regular_method :get_events, [:artist], [] do |response|
+      regular_method :get_events, [:artist], [:mbid, :autocorrect, :limit, :page, :festivalsonly] do |response|
         response.xml['events']['event']
       end
 
-      regular_method :get_top_tags, [:artist], [] do |response|
+      regular_method :get_top_tags, [:artist], [:mbid, :autocorrect] do |response|
         response.xml['toptags']['tag']
       end
 
-      regular_method :get_top_albums, [:artist, :limit], [] do |response|
+      regular_method :get_top_albums, [:artist], [:mbid, :autocorrect, :page, :limit] do |response|
         response.xml['topalbums']['album']
       end
 
-      regular_method :get_images, [:artist, :limit, :order], [] do |response|
+      regular_method :get_images, [:artist], [:page, :limit, :autocorrect, :order] do |response|
         response.xml['images']['image']
       end
 
-      regular_method :search, [:artist], [[:limit, nil], [:page, nil]]
+      regular_method :search, [:artist], [:limit, :page]
     end
   end
 end
