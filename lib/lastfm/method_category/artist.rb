7219fa6c69a00e1d7de6bfd7528a68f405e9cda5
Joshua Knox <joshuaknox@gmail.com>
2012-01-05 17:35:24 -0800

    Update lib/lastfm/method_category/artist.rb

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -4,6 +4,9 @@ class Lastfm
       regular_method :get_events, [:artist], [] do |response|
         response.xml['events']['event']
       end
+      regular_method :get_similar, [:artist], [] do |response|
+        response.xml['similarartists']['artist']
+      end
     end
   end
 end
