3c690a9f7295dc407dd41c04d0661e81121b781f
David J. Brenes <davidjbrenes@gmail.com>
2012-02-28 00:10:55 +0100

    Don't know how to ignore the artist attribute in the response, so we only select those artists which are hashes, not the attribute (which stores the name of the same artist we are querying about)

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -8,7 +8,7 @@ class Lastfm
         response.xml['events']['event']
       end      
       regular_method :get_similar, [:artist], [] do |response|
-        response.xml['similarartists']['artist']
+        response.xml['similarartists']['artist'].select{|o|o.is_a? Hash}
       end
     end
   end
