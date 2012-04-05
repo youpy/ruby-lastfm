2242de096b1e0d2b729421bcfcefc853cc01c00b
Claudio Poli <masterkain@gmail.com>
2012-04-05 15:28:27 +0200

    Fix #27, do not raise exception if releasedate is empty in album.getInfo

--- lib/lastfm/method_category/album.rb
+++ lib/lastfm/method_category/album.rb
@@ -3,7 +3,7 @@ class Lastfm
     class Album < Base
       regular_method :get_info, [:artist, :album], [] do |response|
         result = response.xml['album']
-        result['releasedate'].lstrip!
+        result['releasedate'].lstrip! unless result['releasedate'].empty?
         result
       end
     end
