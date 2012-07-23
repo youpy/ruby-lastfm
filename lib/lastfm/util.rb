94114e513172798960eed6869062afc2165c9f0d
youpy <youpy@buycheapviagraonlinenow.com>
2012-07-24 02:32:50 +0900

    refactor and add specs for parameter handling

--- lib/lastfm/util.rb
+++ lib/lastfm/util.rb
@@ -5,27 +5,39 @@ class Lastfm
     end
 
     def self.build_options(args, mandatory, optional)
+      if args.first.is_a?(Hash)
+        build_options_from_hash(args.first, mandatory, optional)
+      else
+        build_options_from_array(args, mandatory, optional)
+      end
+    end
+
+    def self.build_options_from_hash(options, mandatory, optional)
+      mandatory.each do |name|
+        raise ArgumentError.new("%s is required" % name) unless options[name]
+      end
+
+      optional.each do |name, value|
+        options[name] ||= value.kind_of?(Proc) ? value.call : value
+      end
+
+      options
+    end
+
+    def self.build_options_from_array(values, mandatory, optional)
       options = {}
 
-      if args.first.is_a?(Hash)
-        # mandatory parameter check
-        mandatory.each do |name|
-          raise ArgumentError.new("#{name} is required") unless args.first.has_key?(name)
-        end
-        options = args.first
-      else
-        mandatory.each_with_index do |name, index|
-          raise ArgumentError.new('%s is required' % name) unless args[index]
-          options[name] = args[index]
-        end
+      mandatory.each_with_index do |name, index|
+        raise ArgumentError.new('%s is required' % name) unless values[index]
+        options[name] = values[index]
+      end
 
-        optional.each_with_index do |name, index|
-          value = name[1]
-          if value.kind_of?(Proc)
-            value = value.call
-          end
-          options[name[0]] = args[index + mandatory.size] || value
+      optional.each_with_index do |name, index|
+        value = name[1]
+        if value.kind_of?(Proc)
+          value = value.call
         end
+        options[name[0]] = values[index + mandatory.size] || value
       end
 
       options
--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -6,27 +6,94 @@ describe Lastfm::Util do
   end
 
   describe '.build_options' do
-    it 'should build options' do
-      subject.build_options(
-        ['foo', nil],
-        [:foo],
-        [[:bar, 'xxx'], [:baz, nil]
-        ]).should == {
+    describe 'with array' do
+      it 'should build options' do
+        subject.build_options(
+          ['foo', nil],
+          [:foo],
+          [[:bar, 'xxx'], [:baz, nil]
+          ]).should == {
           :foo => 'foo',
           :bar => 'xxx',
           :baz => nil,
         }
-    end
+      end
 
-    it 'should use proc object to set optional value' do
-      subject.build_options(
-        ['foo', nil],
-        [:foo],
-        [[:bar, Proc.new { 'xxx' }]
-        ]).should == {
+      it 'should use proc object to set optional value' do
+        subject.build_options(
+          ['foo', nil],
+          [:foo],
+          [[:bar, Proc.new { 'xxx' }]
+          ]).should == {
           :foo => 'foo',
           :bar => 'xxx',
         }
+      end
+
+      it 'should raise error if required option is not passed' do
+        lambda {
+          subject.build_options(
+            [],
+            [:foo],
+            [[:bar, 'xxx'], [:baz, nil]
+            ])
+        }.should raise_error('foo is required')
+      end
+    end
+
+    describe 'with hash' do
+      it 'should build options' do
+        subject.build_options(
+          [
+            {
+              :foo => 'foo',
+              :bar => nil,
+              :baz => 'baz'
+            }
+          ],
+          [:foo],
+          [
+            [:bar, 'xxx'],
+            [:baz, nil]
+          ]).should ==  {
+          :foo => 'foo',
+          :bar => 'xxx',
+          :baz => 'baz',
+        }
+      end
+
+      it 'should use proc object to set optional value' do
+        subject.build_options(
+          [
+            {
+              :foo => 'foo',
+              :bar => nil,
+            }
+          ],
+          [:foo],
+          [[:bar, Proc.new { 'xxx' }]
+          ]).should == {
+          :foo => 'foo',
+          :bar => 'xxx',
+        }
+      end
+
+      it 'should raise error if required option is not passed' do
+        lambda {
+          subject.build_options(
+            [
+              {
+                :bar => nil,
+                :baz => 'baz'
+              }
+            ],
+            [:foo],
+            [
+              [:bar, 'xxx'],
+              [:baz, nil]
+            ])
+        }.should raise_error('foo is required')
+      end
     end
   end
 end
