fc40501a0a995d461ca3092adf72bae5deabafd2
Ionut Moraru <morion4000@gmail.com>
2012-11-17 23:33:30 +0200

    Forgot to change the class name to Chart

--- lib/lastfm/method_category/chart.rb
+++ lib/lastfm/method_category/chart.rb
@@ -1,6 +1,6 @@
 class Lastfm
   module MethodCategory
-    class Artist < Base
+    class Chart < Base
       regular_method :get_hyped_artists, [], [[:page, nil], [:limit, nil]] do |response|
         response.xml['artists']['artist']
       end
