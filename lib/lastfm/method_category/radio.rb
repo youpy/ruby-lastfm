f0a0022a1c9a3002a3194a91029f22528b5a087a
youpy <youpy@buycheapviagraonlinenow.com>
2013-01-05 17:10:19 +0900

    clean code

--- lib/lastfm/method_category/radio.rb
+++ lib/lastfm/method_category/radio.rb
@@ -3,11 +3,13 @@ class Lastfm
     class Radio < Base
       write_method :tune, [:station]
       method_with_authentication :get_playlist, [], [] do |response|
-        playlist=response.xml['playlist']
+        playlist = response.xml['playlist']
         tracklist = playlist['trackList']['track']
-        if not tracklist.is_a?(Array)
-          playlist['trackList']['track'] = [ tracklist ]
+
+        unless tracklist.is_a?(Array)
+          playlist['trackList']['track'] = [tracklist]
         end
+
         playlist
       end
     end
--- spec/method_specs/radio_spec.rb
+++ spec/method_specs/radio_spec.rb
@@ -13,20 +13,21 @@ describe '#radio' do
         :station => 'lastfm://globaltags/pop',
       }, :post, true, true).and_return(@ok_response)
       @lastfm.radio.tune("lastfm://globaltags/pop")
-    end    
+    end
   end
-  
+
   describe '#get_playlist' do
     it 'should return some playlist' do
       @lastfm.should_receive(:request).with("radio.getPlaylist", {}, :get, true, true).and_return(make_response('radio_get_playlist'))
       playlist = @lastfm.radio.get_playlist
-      tracklist=playlist["trackList"]["track"]
+      tracklist = playlist["trackList"]["track"]
       tracklist.should be_an_instance_of(Array)
       tracklist[0]['title'].should == "All The Things She Said"
       tracklist[1]['location'].should == "http://play.last.fm/user/bca46e434c3389217ef1b8d20db1690c.mp3"
       tracklist[2]['creator'].should == "Culture Club"
       tracklist[3]['album'].should == "The E.N.D."
     end
+
     it 'should always return playlists with arrays of tracks' do
       @lastfm.should_receive(:request).with("radio.getPlaylist", {}, :get, true, true).and_return(make_response('radio_get_playlist_single_track'))
       playlist = @lastfm.radio.get_playlist
