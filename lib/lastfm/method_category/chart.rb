28e57bf876afe8f3eb13a1f8a6e3b97eb124c936
Rahil Sondhi <rahilsondhi@gmail.com>
2013-02-25 18:17:30 -0500

    Adds more charts

--- lib/lastfm/method_category/chart.rb
+++ lib/lastfm/method_category/chart.rb
@@ -5,8 +5,24 @@ class Lastfm
         response.xml['artists']['artist']
       end
       
+      regular_method :get_hyped_tracks, [], [[:page, nil], [:limit, nil]] do |response|
+        response.xml['tracks']['track']
+      end
+      
+      regular_method :get_loved_tracks, [], [[:page, nil], [:limit, nil]] do |response|
+        response.xml['tracks']['track']
+      end
+      
       regular_method :get_top_artists, [], [[:page, nil], [:limit, nil]] do |response|
         response.xml['artists']['artist']
+      end
+      
+      regular_method :get_top_tags, [], [[:page, nil], [:limit, nil]] do |response|
+        response.xml['tags']['tag']
+      end
+      
+      regular_method :get_top_tracks, [], [[:page, nil], [:limit, nil]] do |response|
+        response.xml['tracks']['track']
       end
     end
   end
