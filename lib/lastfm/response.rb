1a7580156e45ff0c2ae7fb432e3b593cd3e3c2cc
youpy <youpy@buycheapviagraonlinenow.com>
2017-07-05 21:52:32 +0900

    fix #83

--- lib/lastfm/response.rb
+++ lib/lastfm/response.rb
@@ -6,10 +6,12 @@ class Lastfm
     attr_reader :xml
 
     def initialize(body)
+      body = fix_body(body)
+
       @xml = XmlSimple.xml_in(body, 'ForceArray' => ['image', 'tag', 'user', 'event', 'correction'])
     rescue REXML::ParseException
-      @xml = XmlSimple.xml_in(body.encode(Encoding.find("ISO-8859-1"), :undef => :replace), 
-                                          'ForceArray' => ['image', 'tag', 'user', 'event', 'correction'])
+      @xml = XmlSimple.xml_in(body.encode(Encoding.find("ISO-8859-1"), :undef => :replace),
+                              'ForceArray' => ['image', 'tag', 'user', 'event', 'correction'])
     end
 
     def success?
@@ -22,6 +24,20 @@ class Lastfm
 
     def error
       @xml['error']['code'].to_i
+    end
+
+    private
+
+    def fix_body(body)
+      namespace_attr = 'xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/"'
+
+      body.sub(/(<results[^>]*)/) do |str|
+        if str.match(namespace_attr)
+          str
+        else
+          '%s %s' % [str, namespace_attr]
+        end
+      end
     end
   end
 end
